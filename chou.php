bcad48c268bb269cb8d02c105273b6ec2323ccda
lun <2870572765@qq.com>
2019-01-03 16:25:15 +0800

    丑数

---
+++ chou.php
@@ -0,0 +1,25 @@
+<?php
+function num($n){
+	if(!is_numeric($n)){
+		return  "不是一个数字";
+		
+	}
+	$num =$n;
+	while ($n%2==0) {
+		$n/=2;
+	}
+	while ($n%3==0) {
+		$n/=3;
+	}
+	while ($n%5==0) {
+		$n/=5;
+	}
+	if($n==1){
+		echo $num.":是丑数";
+	}else{
+		echo $num.":不是丑数";
+	}
+}
+
+echo num('asd');
+?>
